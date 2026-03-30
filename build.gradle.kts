c16fbd385bd20e85668aae2d241a426085843e95
Mark Novak <67005577+tavanuka@users.noreply.github.com>
2026-03-30 17:02:49 +0200

    refactor: remove latest compatible build (#82)
    * refactor: remove latest compatible build
    
    * refactor: remove latest compatible build
    
    * build: bump plugin version
    
    * chore: update changelog

--- build.gradle.kts
+++ build.gradle.kts
@@ -84,7 +84,6 @@ intellijPlatform {
 
         ideaVersion {
             sinceBuild = providers.gradleProperty("pluginSinceBuild")
-            untilBuild = providers.gradleProperty("pluginUntilBuild")
         }
     }
 
